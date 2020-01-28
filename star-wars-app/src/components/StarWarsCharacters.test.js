b164129cd07b9a47d624cfb023c80941c81c55ab
Jasmine Terry <jasmine.terry91@gmail.com>
2020-01-28 00:00:44 -0500

    buttons rendering test work

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -2,13 +2,25 @@
 //test api call
 //test next & prev button clicks
 import React from 'react';
-import { render, fireEvent, wait } from '@testing-library/react';
+import { render, fireEvent, wait, getByText } from '@testing-library/react';
 import StarWarsCharacters from './StarWarsCharacters';
 
 
 test (' renders character component', async()=>{
-    const { findByText, findByAltText } = render(<StarWarsCharacters/>);
+    const { findByText, getByText } = render(<StarWarsCharacters/>);
     findByText(/Darth Vader/i);
     findByText(/leia Organa/i);
     
-})
+//buttons tests
+
+const nextButton = getByText(/Next/i);
+fireEvent.click(nextButton);
+
+expect(nextButton.type).toBe('submit');
+
+const backButton = getByText(/Previous/i);
+fireEvent.click(backButton);
+
+expect(backButton.type).toBe('submit');
+
+});
